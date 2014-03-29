0f08f91ffca616ac4db53a9effdcb74769f7c64d
peluche <peluche@example.com>
2014-03-30 04:10:18 +0800

    admin for event

--- rails_app/app/admin/event.rb
+++ rails_app/app/admin/event.rb
@@ -1,5 +1,18 @@
 ActiveAdmin.register Event do
   permit_params :title, :description, :location, :event_date, :slot, :price, :pic
+
+  form :html => { :enctype => "multipart/form-data" } do |f|  
+    f.inputs "Events" do
+      f.input :title
+      f.input :description
+      f.input :event_date, :as => :datepicker
+      f.input :location
+      f.input :slot
+      f.input :price
+      f.input :pic, :as => :file
+    end
+    f.actions
+  end
   
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
