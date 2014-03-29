a5de318be8d62e525e88b0a97e8dd7affbf09b88
k <k@itsium.cn>
2014-03-29 18:32:45 +0800

    init

---
+++ rails_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Whilewechat::Application.config.secret_key_base = 'f082ef5aff03ad6997fe6fa803fd000c10e341ae0d2f8648e7bb436f5a05f5952e29c78a8b76789c8e81414e3722db4ee5b3b3d598baeb57ad03a8949b34cbec'
