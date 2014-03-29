30a1b1c9e5624ea2496bf07ca5db73876b16ef62
peluche <peluche@example.com>
2014-03-30 02:12:30 +0800

    before breaking point

---
+++ rails_app/db/schema.rb
@@ -0,0 +1,52 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20140329163344) do
+
+  create_table "events", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "location"
+    t.datetime "event_date"
+    t.integer  "slot"
+    t.float    "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "subscriptions", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "subscriptions", ["event_id"], name: "index_subscriptions_on_event_id"
+  add_index "subscriptions", ["user_id"], name: "index_subscriptions_on_user_id"
+
+  create_table "users", force: true do |t|
+    t.string   "fullname"
+    t.string   "company"
+    t.string   "email"
+    t.string   "school"
+    t.string   "grade"
+    t.string   "city"
+    t.string   "country"
+    t.string   "phone"
+    t.text     "about"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "linkedin"
+  end
+
+end
