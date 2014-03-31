669229969d81e77cb2b7dae5333e49721cd948f8
k <k@itsium.cn>
2014-03-31 17:06:34 +0800

    pumaconf

--- rails_app/config/puma.rb
+++ rails_app/config/puma.rb
@@ -3,8 +3,8 @@ rails_env = ENV['RAILS_ENV'] || 'development'
 threads 4,4
 
 bind  "unix:///var/run/whilewechat.sock"
-pidfile "/srv/www/w42.itsium.cn/whilewechat/tmp/puma.pid"
-state_path "/srv/www/w42.itsium.cn/whilewechat/tmp/puma.state"
+pidfile "/srv/www/w42.itsium.cn/rails_app/tmp/puma.pid"
+state_path "/srv/www/w42.itsium.cn/rails_app/tmp/puma.state"
 
 activate_control_app
 
